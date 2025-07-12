5b460eb7508a2848285c4edb956f6b2d4c05155a
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2025-07-12 21:28:24 +0530

    Addind debug file to check in debug issue

--- post_modified_data_to_server.py
+++ post_modified_data_to_server.py
@@ -6,8 +6,12 @@ import time
 #CREATING DATAFRAME FROM CSV DATA.
 df=pd.read_csv('Menu_data.csv')
 
+
+log_file=open("upload_log.txt","a")
+
 api_url="https://httpbin.org/post" 
 
+msg=''
 for _,row in df.iterrows():
     load={
         "id":row["id"],
@@ -20,12 +24,14 @@ for _,row in df.iterrows():
     try:
         response=requests.post(api_url,json=load)
         if response.status_code==200:
-            print(f'Uploaded:{row['title']}')
+            msg=msg+f'Uploaded:{row['title']}'+'\n'
         else :
-            print(f'error occured : {response.status_code}')
+            msg=print(f'error occured : {response.status_code}')
     except Exception as e:
         print(f'Exception : {e}')
     
+print(msg)
+log_file.write(msg)
     
         
 
