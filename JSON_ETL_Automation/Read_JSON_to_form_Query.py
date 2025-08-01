0a8e4b6cd03c6682117c5240dfd46a9a5ff12bee
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2025-08-01 17:51:38 +0530

    Added Code to read the Select part of the query

--- JSON_ETL_Automation/Read_JSON_to_form_Query.py
+++ JSON_ETL_Automation/Read_JSON_to_form_Query.py
@@ -10,5 +10,12 @@ with open('Query_Parts.json','r') as file:
 query_generation=f"MERGE INTO {data['Query_Merge_Select_Part']['Target_Database']}.{data['Query_Merge_Select_Part']['Target_Schema']}.{data['Query_Merge_Select_Part']['Target_Table']} as A using ("
 print(query_generation)
 
+select_clauses=[]
 
+for col in data['ETL_Logic']['Columns']:
+    clause = f"{col['Source_Column_Name']}.{col['Logic']} AS {col['Target_Column_Name']}"
+    select_clauses.append(clause)
+
+select_statement = "SELECT " + ",\n       ".join(select_clauses)
+print(select_statement)
 
